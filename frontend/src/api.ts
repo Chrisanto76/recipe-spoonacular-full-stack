30ef7a3117b0d6b8fc9d9fd2e484003e47349cbe
Chrisanto76 <antonypoure2@gmail.com>
2023-11-06 21:39:47 +0100

    add api.ts and handleSearchSubmit fct

---
+++ frontend/src/api.ts
@@ -0,0 +1,11 @@
+export const searchRecipes = async (searchTerm: string, page: number) => {
+	const baseUrl = new URL('http://localhost:4000/api/recipes/search');
+	baseUrl.searchParams.append('searchTerm', searchTerm);
+	baseUrl.searchParams.append('page', String(page));
+
+	const response = await fetch(baseUrl);
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	return await response.json();
+};
